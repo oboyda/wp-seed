3e295f5dba5901b4bcc809b95a4f210541b6ddc5
Oleksiy Boyda <oleksiyboyda@Oleksiys-MacBook-Pro.local>
2021-05-24 18:19:28 +0200

    v1.5-dev

--- src/classes/View.php
+++ src/classes/View.php
@@ -6,8 +6,9 @@ if(!class_exists('\WPSEED\View'))
 {
     class View {
         
+        var $id;
         var $args;
-        
+            
         /*
         * Construct the View object
         *
@@ -15,10 +16,27 @@ if(!class_exists('\WPSEED\View'))
         * @param array $args Default arguments to be used in the template.
         */
 
-        public function __construct($args=[], $default_args=[]){
-            
+        public function __construct($args=[], $default_args=[])
+        {
+            $this->id = $this->genId();
             $this->args = empty($default_args) ? $args : wp_parse_args($args, $default_args);
         }
-        
+
+        public function getAjaxUrl()
+        {
+            return admin_url('admin-ajax.php');
+        }
+    
+        private function genId()
+        {
+            $chars = str_shuffle('abcdefghijklmnopqrstuvwxyz1234567890');
+            return 'view-' . substr(str_shuffle($chars), 0, 10);
+        }
+    
+        public function getId()
+        {
+            return $this->id;
+        }
+            
     }
 }
