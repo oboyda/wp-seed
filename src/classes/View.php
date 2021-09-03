94b51abe4e9cf1ca3f702c97bd988477fd728c9c
oleksiyboyda <oleksiyboyda@oleksiys-mbp.home>
2021-09-03 16:43:10 +0200

    1.14.0

--- src/classes/View.php
+++ src/classes/View.php
@@ -15,27 +15,70 @@ if(!class_exists(__NAMESPACE__ . '\View'))
         * @param array $args Arguments to be used in the template. Will be merged with the default arguments.
         * @param array $args Default arguments to be used in the template.
         */
-
         public function __construct($args=[], $default_args=[])
         {
             $this->id = $this->genId();
             $this->args = empty($default_args) ? $args : wp_parse_args($args, $default_args);
         }
 
+        /* 
+         * Return WP ajax URL
+         * 
+         * @return str
+         */
         public function getAjaxUrl()
         {
             return admin_url('admin-ajax.php');
         }
     
+        /* 
+         * Generates random ID
+         * 
+         * @return str
+         */
         private function genId()
         {
             $chars = str_shuffle('abcdefghijklmnopqrstuvwxyz1234567890');
             return 'view-' . substr(str_shuffle($chars), 0, 10);
         }
     
+        /* 
+         * Get random ID
+         * 
+         * @return str
+         */
         public function getId()
         {
             return $this->id;
+        }
+        
+        /* 
+         * Get current view classes
+         * 
+         * @param str|array $merge_classes
+         * @return str
+         */
+        public function getClasses($merge_classes=null)
+        {
+            $classes = [
+                'view'
+            ];
+            
+            $view_name = str_replace('_', '-', basename(str_replace('\\', '/', get_called_class())));
+            
+            $classes[] = $view_name;
+            
+            if(isset($merge_classes))
+            {
+                if(is_string($merge_classes))
+                {
+                    $merge_classes = explode(' ', $merge_classes);
+                }
+                
+                $classes = array_merge($classes, explode(' ', $merge_classes));
+            }
+            
+            return implode(' ', $classes);
         }
     }
 }
