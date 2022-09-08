b511e9b5e6bbaf097a91169b142ec1b5a7acb184
Oleksiy Boyda <oleksiyboyda@Oleksiys-MacBook-Pro.local>
2022-09-08 18:18:35 +0200

    updates

--- src/classes/Action.php
+++ src/classes/Action.php
@@ -2,178 +2,181 @@
 
 namespace WPSEED;
 
-class Action 
+if(!class_exists(__NAMESPACE__ . '\Action'))
 {
-    protected $status;
-    protected $error_fields;
-    protected $error_messages;
-    protected $success_messages;
-    protected $messages;
-    protected $values;
-    protected $redirect;
-    protected $reload;
-
-    protected $req;
-
-    function __construct()
+    class Action 
     {
-        $this->status = true;
-        $this->error_fields = [];
-        $this->error_messages = [];
-        $this->success_messages = [];
-        $this->messages = '';
-        $this->values = [];
-        $this->redirect = '';
-        $this->reload = false;
+        protected $status;
+        protected $error_fields;
+        protected $error_messages;
+        protected $success_messages;
+        protected $messages;
+        protected $values;
+        protected $redirect;
+        protected $reload;
 
-        $this->req = new Req();
-    }
+        protected $req;
 
-    protected function getRequestTypeProps($props_config)
-    {
-        $req_props = [];
-
-        foreach($props_config as $key => $prop_config)
+        function __construct()
         {
-            $cast = isset($prop_config['cast']) ? $prop_config['cast'] : 'text';
-            $req_value = $this->getReq($key, $cast);
-            if(isset($req_value))
+            $this->status = true;
+            $this->error_fields = [];
+            $this->error_messages = [];
+            $this->success_messages = [];
+            $this->messages = '';
+            $this->values = [];
+            $this->redirect = '';
+            $this->reload = false;
+
+            $this->req = new Req();
+        }
+
+        protected function getRequestTypeProps($props_config)
+        {
+            $req_props = [];
+
+            foreach($props_config as $key => $prop_config)
             {
-                $req_props[$key] = $req_value;
+                $cast = isset($prop_config['cast']) ? $prop_config['cast'] : 'text';
+                $req_value = $this->getReq($key, $cast);
+                if(isset($req_value))
+                {
+                    $req_props[$key] = $req_value;
+                }
+            }
+
+            return $req_props;
+        }
+
+        protected function setStatus($status)
+        {
+            $this->status = (bool)$status;
+        }
+
+        protected function setRedirect($redirect)
+        {
+            $this->redirect = $redirect;
+        }
+
+        protected function setReload($reload)
+        {
+            $this->reload = (bool)$reload;
+        }
+
+        protected function addErrorField($field)
+        {
+            if(!in_array($field, $this->error_fields))
+            {
+                $this->error_fields[] = $field;
             }
         }
 
-        return $req_props;
-    }
-
-    protected function setStatus($status)
-    {
-        $this->status = (bool)$status;
-    }
-
-    protected function setRedirect($redirect)
-    {
-        $this->redirect = $redirect;
-    }
-
-    protected function setReload($reload)
-    {
-        $this->reload = (bool)$reload;
-    }
-
-    protected function addErrorField($field)
-    {
-        if(!in_array($field, $this->error_fields))
+        protected function removeErrorField($field)
         {
-            $this->error_fields[] = $field;
-        }
-    }
+            $i = array_search($field, $this->error_fields);
 
-    protected function removeErrorField($field)
-    {
-        $i = array_search($field, $this->error_fields);
-
-        if($i !== false)
-        {
-            unset($this->error_fields[$i]);
-        }
-    }
-
-    protected function checkErrorFields($fields, $required_keys, $respond_on_errors=false)
-    {
-        $empty_fields = Utils::checkArrayEmptyVals($fields, $required_keys);
-
-        if($empty_fields)
-        {
-            foreach($empty_fields as $empty_field)
+            if($i !== false)
             {
-                $this->addErrorField($empty_field);
+                unset($this->error_fields[$i]);
             }
         }
 
-        if($respond_on_errors && $this->hasErrors())
+        protected function checkErrorFields($fields, $required_keys, $respond_on_errors=false)
         {
-            $this->respond();
-        }
-    }
+            $empty_fields = Utils::checkArrayEmptyVals($fields, $required_keys);
 
-    protected function checkCurrentUserCapability($capability)
-    {
-        if(!current_user_can($capability))
-        {
-            $this->setStatus(false);
-            $this->addErrorMessage(__('Current user is not allowed to perform this action.', 'wpseed'));
-            $this->respond();
-        }
-    }
+            if($empty_fields)
+            {
+                foreach($empty_fields as $empty_field)
+                {
+                    $this->addErrorField($empty_field);
+                }
+            }
 
-    protected function addSuccessMessage($message)
-    {
-        if(!in_array($message, $this->success_messages))
-        {
-            $this->success_messages[] = $message;
-            $this->messages .= $this->wrapResponseMessages($message, 'success');
-        }
-    }
-
-    protected function addErrorMessage($message)
-    {
-        if(!in_array($message, $this->error_messages))
-        {
-            $this->error_messages[] = $message;
-            $this->messages .= $this->wrapResponseMessages($message, 'error');
-        }
-    }
-
-    protected function wrapResponseMessages($messages, $type='success')
-    {
-        $w_messages = [];
-        foreach((array)$messages as $message)
-        {
-            $w_messages[] = '<p class="message ' . $type . '">' . $message . '</p>';
-        }
-        return implode('', $w_messages);
-    }
-
-    protected function setValue($key, $value)
-    {
-        $this->values[$key] = $value;
-    }
-
-    protected function getReq($key, $san='text', $default=null)
-    {
-        return $this->req->get($key, $san, $default);
-    }
-
-    protected function hasErrors()
-    {
-        return (!$this->status || !empty($this->error_fields));
-    }
-
-    protected function respond($resp=[])
-    {
-        if($this->error_fields)
-        {
-            $this->setStatus(false);
-            $this->addErrorMessage(__('Please, check the required fields.', 'wpseed'));
+            if($respond_on_errors && $this->hasErrors())
+            {
+                $this->respond();
+            }
         }
 
-        if(!$this->status && empty($this->messages))
+        protected function checkCurrentUserCapability($capability)
         {
-            $this->addErrorMessage(__('Something went wrong. Please, try again later.', 'wpseed'));
+            if(!current_user_can($capability))
+            {
+                $this->setStatus(false);
+                $this->addErrorMessage(__('Current user is not allowed to perform this action.', 'wpseed'));
+                $this->respond();
+            }
         }
 
-        $resp = wp_parse_args($resp, [
-            'status' => $this->status,
-            'error_fields' => $this->error_fields,
-            'error_messages' => $this->error_messages,
-            'success_messages' => $this->success_messages,
-            'messages' => $this->messages,
-            'values' => $this->values,
-            'redirect' => $this->redirect,
-            'reload' => $this->reload
-        ]);
+        protected function addSuccessMessage($message)
+        {
+            if(!in_array($message, $this->success_messages))
+            {
+                $this->success_messages[] = $message;
+                $this->messages .= $this->wrapResponseMessages($message, 'success');
+            }
+        }
 
-        wp_send_json($resp);
+        protected function addErrorMessage($message)
+        {
+            if(!in_array($message, $this->error_messages))
+            {
+                $this->error_messages[] = $message;
+                $this->messages .= $this->wrapResponseMessages($message, 'error');
+            }
+        }
+
+        protected function wrapResponseMessages($messages, $type='success')
+        {
+            $w_messages = [];
+            foreach((array)$messages as $message)
+            {
+                $w_messages[] = '<p class="message ' . $type . '">' . $message . '</p>';
+            }
+            return implode('', $w_messages);
+        }
+
+        protected function setValue($key, $value)
+        {
+            $this->values[$key] = $value;
+        }
+
+        protected function getReq($key, $san='text', $default=null)
+        {
+            return $this->req->get($key, $san, $default);
+        }
+
+        protected function hasErrors()
+        {
+            return (!$this->status || !empty($this->error_fields));
+        }
+
+        protected function respond($resp=[])
+        {
+            if($this->error_fields)
+            {
+                $this->setStatus(false);
+                $this->addErrorMessage(__('Please, check the required fields.', 'wpseed'));
+            }
+
+            if(!$this->status && empty($this->messages))
+            {
+                $this->addErrorMessage(__('Something went wrong. Please, try again later.', 'wpseed'));
+            }
+
+            $resp = wp_parse_args($resp, [
+                'status' => $this->status,
+                'error_fields' => $this->error_fields,
+                'error_messages' => $this->error_messages,
+                'success_messages' => $this->success_messages,
+                'messages' => $this->messages,
+                'values' => $this->values,
+                'redirect' => $this->redirect,
+                'reload' => $this->reload
+            ]);
+
+            wp_send_json($resp);
+        }
     }
 }
--- src/classes/Deps.php
+++ src/classes/Deps.php
@@ -2,53 +2,56 @@
 
 namespace WPSEED;
 
-class Deps
+if(!class_exists(__NAMESPACE__ . '\Deps'))
 {
-    protected $deps;
-    protected $args;
-
-    var $notices;
-    
-    public function __construct($deps=[], $args=[])
+    class Deps
     {
-        $this->deps = $deps;
-        $this->args = wp_parse_args($args, [
-            'plugin_name' => __('Custom plugin', 'wpseed')
-        ]);
+        protected $deps;
+        protected $args;
 
-        $this->notices = [];
+        var $notices;
         
-        add_action('admin_notices', [$this, 'printNotices']);
-    }
-    
-    public function check()
-    {
-        include_once(ABSPATH . 'wp-admin/includes/plugin.php');
-
-        $check = true;
-
-        foreach($this->deps as $dep)
+        public function __construct($deps=[], $args=[])
         {
-            if(!is_plugin_active($dep))
-            {
-                $check = false;
+            $this->deps = $deps;
+            $this->args = wp_parse_args($args, [
+                'plugin_name' => __('Custom plugin', 'wpseed')
+            ]);
 
-                $notice  = '<div class="notice notice-warning is-dismissible">';
-                    $notice .= '<p>' . sprintf(__('%s requires %s to work properly.', 'wpseed'), $this->args['plugin_name'], $dep) . '</p>';
-                $notice .= '</div>';
-                
-                $this->notices[] = $notice;
-            }
+            $this->notices = [];
+            
+            add_action('admin_notices', [$this, 'printNotices']);
         }
-
-        return $check;
-    }
-    
-    public function printNotices()
-    {
-        if(!empty($this->notices))
+        
+        public function check()
         {
-            echo implode(PHP_EOL, $this->notices);
+            include_once(ABSPATH . 'wp-admin/includes/plugin.php');
+
+            $check = true;
+
+            foreach($this->deps as $dep)
+            {
+                if(!is_plugin_active($dep))
+                {
+                    $check = false;
+
+                    $notice  = '<div class="notice notice-warning is-dismissible">';
+                        $notice .= '<p>' . sprintf(__('%s requires %s to work properly.', 'wpseed'), $this->args['plugin_name'], $dep) . '</p>';
+                    $notice .= '</div>';
+                    
+                    $this->notices[] = $notice;
+                }
+            }
+
+            return $check;
+        }
+        
+        public function printNotices()
+        {
+            if(!empty($this->notices))
+            {
+                echo implode(PHP_EOL, $this->notices);
+            }
         }
     }
 }
